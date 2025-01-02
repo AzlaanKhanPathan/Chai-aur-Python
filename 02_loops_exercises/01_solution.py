2282c107e0a6451c51d087c323708e9b3e508f89
Azlaan Khan Pathan <144579915+AzlaanPathan@users.noreply.github.com>
2025-01-02 09:44:07 +0530

    this is exercise 2

---
+++ 02_loops_exercises/01_solution.py
@@ -0,0 +1,8 @@
+numbers = [1, -2, 3, -4, 5, 6, -7, -8, 9, 10]
+
+postivie_num_count = 0 
+
+for num in numbers:
+    if num > 0:
+        postivie_num_count += 1 
+print("The final count of positive numbers is : ", postivie_num_count)
---
+++ 02_loops_exercises/04_solution.py
@@ -0,0 +1,8 @@
+sample_string = "Python"
+
+reversed_str = ""
+
+for char in sample_string:
+     reversed_str = char + reversed_str
+
+print(reversed_str)
