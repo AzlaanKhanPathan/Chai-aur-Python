2f1df3a82325ebde96e968dd8239284366a171e9
Azlaan Khan Pathan <akpglb@gmail.com>
2025-01-15 09:47:04 +0530

    updated the file

--- 08_handling_apis/free_api_products.py
+++ 08_handling_apis/free_api_products.py
@@ -1,6 +1,6 @@
 import requests
 
-def fetch_product():
+def fetch_random_product():
     url = "https://api.freeapi.app/api/v1/public/randomproducts"
     response = requests.get(url)
     data = response.json()
@@ -14,7 +14,7 @@ def fetch_product():
 
 def main():
     try:
-        product = fetch_product()
+        product = fetch_random_product()
         print(f"The Product is: {product}")
     except Exception as e:
         print(str(e))
